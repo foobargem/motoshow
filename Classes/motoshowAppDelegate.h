7b29755bb254b1d8f82fbd1faa49e3356c75340f
Ashton Kim <ashton.mkim@gmail.com>
2011-03-02 18:20:36 +0900

    init repository.

---
+++ Classes/motoshowAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  motoshowAppDelegate.h
+//  motoshow
+//
+//  Created by 김민수 on 11. 3. 2..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface motoshowAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  motoshow
+//
+//  Created by 김민수 on 11. 3. 2..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"motoshowAppDelegate");
+    [pool release];
+    return retVal;
+}
