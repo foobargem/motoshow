73ffb15338501ae17346e591edc0a83df433eeac
Ashton Kim <ashton.mkim@gmail.com>
2011-03-03 18:30:27 +0900

    added rails app for sass

---
+++ motoshow_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotoshowRails::Application.config.secret_token = 'c4acfa7ffc1a2b0fa1b2bb409c85f750c95e516e26b8f5289369a813e4384a1881f568c7edc34f168024bc0d3dcf4e96229d3643f7534de5c79b2aceb4db9279'
