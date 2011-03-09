fb012f5413c29c08b3013135a0b9cfefe7a79958
Eunchul Jung <agfe09@gmail.com>
2011-03-09 10:45:56 +0900

    update: rails default page

--- motoshow_rails/app/controllers/main_controller.rb
+++ motoshow_rails/app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
 
   def index
-    #redirect_to "/before/index.html"
-    redirect_to "/ing/index.html"
+    redirect_to "/before/index.html"
+    #redirect_to "/ing/index.html"
   end
 
 end
