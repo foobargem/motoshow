70a2f62175cac06856d41d5b944cb4d8484a1b4d
Eunchul Jung <agfe09@gmail.com>
2011-03-14 10:23:30 +0900

    update: banner

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -2,12 +2,23 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 var SMS = {
-  //baseURL: "http://localhost:3001"
-  baseURL: "http://10.1.2.139:3001"
+  baseURL: "http://localhost:3001"
   //baseURL: "http://2011sms.top-rider.com"
 };
 
 SMS.API = {
+  EventApplicant: {
+    newURL: function() {
+      return SMS.baseURL + "/events/1/applicants";
+    }
+  },
+
+  Banner: {
+    baseAdURL: function() {
+      return SMS.baseURL + "/banners/base_ad.json";
+    }
+  },
+
   Company: {
     indexURL: function(category) {
       return SMS.baseURL + '/companies.json?category=' + category;
@@ -65,4 +76,53 @@ SMS.API = {
   }
 
 };
+
+
+
+// banner
+
+function Banner(element) {
+  this.title = "";
+  this.content = "";
+  this.landing_url = "";
+  this.embed_code = "";
+  this.element = element;
+
+  var that = this;
+
+  if (!window.plugins || !window.plugins.childBrowser) {
+    ChildBrowser.install();
+  }
+
+  jQuery.ajax({
+    url: SMS.API.Banner.baseAdURL(),
+    type: 'GET',
+    success: function(msg){
+      that.title = msg.title;
+      that.content = msg.content;
+      that.landing_url = msg.landing_url;
+      that.embed_code = msg.embed_code;
+      that.display();
+    }
+  });
+}
+
+
+Banner.prototype.display = function() {
+  $(this.element).html(this.embed_code);
+  this.forceSetStyle();
+  this.addEventListener();
+}
+
+Banner.prototype.forceSetStyle = function() {
+  $(this.element).css("padding", 0);
+}
+
+Banner.prototype.addEventListener = function() {
+  var that = this;
+  $(this.element + " a").bind("click", this, function(event){
+    event.preventDefault();
+    window.plugins.childBrowser.showWebPage(that.landing_url);
+  });
+}
 
